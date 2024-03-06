7b9c6f2df41a3a3cb3a3473959788fd0e2187db6
Reza <reza.abbasi.sut@gmail.com>
2024-03-06 10:13:17 +0330

    move hijack url to admin

--- _base/urls.py
+++ _base/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('api/v2/stake/', include('stake.urls_v2')),
     path('api/v1/gamify/', include('gamify.urls')),
     path('api/', include('ledger.urls')),
-    path('hijack/', include('hijack.urls')),
+    path('admin/hijack/', include('hijack.urls')),
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 
     path('tinymce/', include('tinymce.urls')),
