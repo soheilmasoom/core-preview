c660ebb0a17d2d2d2aa0bf6dfaf18c7ea3a53bff
Reza <reza.abbasi.sut@gmail.com>
2024-03-09 12:06:39 +0330

    make ref_id editable

--- financial/admin.py
+++ financial/admin.py
@@ -115,7 +115,7 @@ class FiatWithdrawRequestAdmin(SimpleHistoryAdmin):
     readonly_fields = (
         'created', 'bank_account', 'amount', 'get_withdraw_request_iban', 'fee_amount', 'get_risks',
         'get_withdraw_request_user', 'get_withdraw_request_receive_time', 'get_user', 'login_activity',
-        'get_withdraw_request_receive_time', 'get_withdraw_request_withdraw_time', 'status', 'ref_id',
+        'get_withdraw_request_receive_time', 'get_withdraw_request_withdraw_time', 'status',
         'accepted_by', 'accepted_datetime'
     )
     search_fields = ('bank_account__iban', 'bank_account__user__phone', 'group_id', 'ref_id')
