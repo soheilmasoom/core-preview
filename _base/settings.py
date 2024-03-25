3e1da27fbe5349654f5453483ab65937275b479f
Reza <reza.abbasi.sut@gmail.com>
2024-03-25 19:53:41 +0330

    decrease throtle rate

--- _base/settings.py
+++ _base/settings.py
@@ -287,8 +287,10 @@ REST_FRAMEWORK = {
         'sustained': '500/day',
         # 'burst_api': '40/min',
         # 'sustained_api': '20000/day',
-        'burst_api': '200/min',
-        'sustained_api': '200000/day',
+        # 'burst_api': '200/min',
+        # 'sustained_api': '200000/day',
+        'burst_api': '50/min',
+        'sustained_api': '10000/day',
     },
     'EXCEPTION_HANDLER': 'accounts.throttle.custom_exception_handler'
 }
