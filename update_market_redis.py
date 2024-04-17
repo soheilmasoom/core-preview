09320b2d94adcba56ee5345331197399d3fd2aae
Sajjad-Khoshdooni <sijalkhf@gmail.com>
2024-04-17 09:44:07 +0330

    fix

--- update_market_redis.py
+++ update_market_redis.py
@@ -33,13 +33,13 @@ def main():
                 per_symbol_orders[order['symbol'], order['side']].append(order)
 
             for symbol in PairSymbol.objects.filter(enable=True):
-                bids = Order.quantize_values(symbol, per_symbol_orders.get((symbol.id, BUY), [])[:60])
-                asks = Order.quantize_values(symbol, per_symbol_orders.get((symbol.id, SELL), [])[:60])
+                bids = Order.quantize_values(symbol, per_symbol_orders.get((symbol.id, BUY), []))
+                asks = Order.quantize_values(symbol, per_symbol_orders.get((symbol.id, SELL), []))
 
                 depth = {
                     'symbol': symbol.name,
-                    'bids': Order.get_formatted_orders(bids, symbol, BUY),
-                    'asks': Order.get_formatted_orders(asks, symbol, SELL),
+                    'bids': Order.get_formatted_orders(bids, symbol, BUY)[:60],
+                    'asks': Order.get_formatted_orders(asks, symbol, SELL)[:60],
                 }
                 pipeline.hset('market_depth_snapshot', symbol.name, msgpack.packb(depth))
                 pipeline.set('market_depth_snapshot_liveness', 1, 60)
