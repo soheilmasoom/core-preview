57b0a7a9b5d567e4464ce59f9b7d5e863c825921
Reza <reza.abbasi.sut@gmail.com>
2024-05-20 16:26:15 +0330

    fix bug in firebase push token

--- accounts/utils/push_notif.py
+++ accounts/utils/push_notif.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Tuple
 
 import requests
 from decouple import config
@@ -10,14 +11,14 @@ from accounts.models import User
 logger = logging.getLogger(__name__)
 
 
-def _get_access_token():
+def _get_access_token() -> Tuple[str, str]:
     scopes = ['https://www.googleapis.com/auth/firebase.messaging']
-    firebase_json = json.loads(config('FIREBASE_SECRET_JSON', ''))
+    firebase_dict = json.loads(config('FIREBASE_SECRET_JSON', ''))
 
-    credentials = ServiceAccountCredentials._from_parsed_json_keyfile(firebase_json, scopes)
+    credentials = ServiceAccountCredentials._from_parsed_json_keyfile(firebase_dict, scopes)
     access_token_info = credentials.get_access_token()
 
-    return access_token_info.access_token
+    return firebase_dict['project_id'], access_token_info.access_token
 
 
 def send_push_notif_to_user(user: User, title: str, body: str, image: str = None, link: str = None):
@@ -48,10 +49,12 @@ def send_push_notif(token: str, title: str, body: str, image: str = None, link: 
             }
         }
 
+    project_id, access_token = _get_access_token()
+
     resp = requests.post(
-        url='https://fcm.googleapis.com/v1/projects/raastin-7203e/messages:send',
+        url=f'https://fcm.googleapis.com/v1/projects/{project_id}/messages:send',
         headers={
-            'Authorization': 'Bearer ' + _get_access_token(),
+            'Authorization': 'Bearer ' + access_token,
             'Content-Type': 'application/json; UTF-8',
         },
         json={
