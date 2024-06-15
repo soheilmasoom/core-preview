8ec8952e41a48ea129807b5fc8afeafee9304389
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-06-15 18:33:38 +0330

    fix: 🐛 use auth for backoffice

--- multimedia/utils/backoffice_content.py
+++ multimedia/utils/backoffice_content.py
@@ -1,25 +1,29 @@
 import requests
 from django.conf import settings
 
-def create_coin_content(coin_name):
-    data = {
-        'coinName': coin_name,
-    }
-    url = settings.BACKOFFICE_BASE_URL + '/api/content-creator/coin'
-    resp = requests.post(url=url, data=data, timeout=60)
-    return resp.status_code, resp.json(),
+class BackofficeContent:
+    BASE_URL = settings.BACKOFFICE_BASE_URL
+    headers={'Authorization': settings.BACKOFFICE_TOKEN}
+
+    def create_coin_content(self, coin_name):
+        data = {
+            'coinName': coin_name,
+        }
+        url = self.BASE_URL + '/api/content-creator/coin'
+        resp = requests.post(url=url, headers=self.headers, data=data, timeout=60)
+        return resp.status_code, resp.json(),
 
 
-def update_coin_content(coin_name):
-    data = {
-        'coinName': coin_name,
-    }
-    url = settings.BACKOFFICE_BASE_URL + '/api/content-creator/coin'
-    resp = requests.patch(url=url, data=data, timeout=60)
-    return resp.status_code, resp.json(),
+    def update_coin_content(self, coin_name):
+        data = {
+            'coinName': coin_name,
+        }
+        url = self.BASE_URL + '/api/content-creator/coin'
+        resp = requests.patch(url=url, headers=self.headers, data=data, timeout=60)
+        return resp.status_code, resp.json(),
 
 
-def get_coin_content(coin_name):
-    url = settings.BACKOFFICE_BASE_URL + '/api/content-creator/coin/' + coin_name
-    resp = requests.get(url=url, timeout=60)
-    return resp.status_code, resp.json(),
+    def get_coin_content(self, coin_name):
+        url = self.BASE_URL + '/api/content-creator/coin/' + coin_name
+        resp = requests.get(url=url, headers=self.headers, timeout=60)
+        return resp.status_code, resp.json(),
