964df954d75be9787593979e29734cd5a687f797
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-06-26 10:29:53 +0330

    fix: 🐛 swagger http request to https request

--- _base/urls.py
+++ _base/urls.py
@@ -26,6 +26,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    url=settings.HOST_URL,
     permission_classes=(permissions.AllowAny,),
 )
 
