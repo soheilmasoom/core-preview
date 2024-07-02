328ef92158f05f18dc24bdd94de291181e369d2c
Reza <reza.abbasi.sut@gmail.com>
2024-07-02 18:13:17 +0330

    fix wallet pipeline

--- ledger/tests/test_wallet.py
+++ ledger/tests/test_wallet.py
@@ -1,10 +1,11 @@
+from decimal import Decimal
 from uuid import uuid4
 
 from django.db import IntegrityError
 from django.test import TestCase
 
 from accounts.models import Account
-from ledger.models import Asset, Trx
+from ledger.models import Asset, Trx, BalanceLock
 from ledger.utils.test import new_account
 from ledger.utils.wallet_pipeline import WalletPipeline
 
@@ -13,6 +14,7 @@ class WalletTestCase(TestCase):
     def setUp(self):
         self.account = new_account()
         self.account2 = new_account()
+        self.irt = Asset.get(Asset.IRT)
         self.usdt = Asset.get(Asset.USDT)
         self.wallet = self.usdt.get_wallet(self.account)
         self.wallet2 = self.usdt.get_wallet(self.account2)
@@ -191,3 +193,51 @@ class WalletTestCase(TestCase):
 
         self.assertEqual(sender.balance, 5)
         self.assertEqual(receiver.balance, 15)
+
+    def test_trade_simulation(self):
+        acc1 = new_account()
+        acc2 = new_account()
+        acc3 = new_account()
+        wallet_irt1 = self.irt.get_wallet(acc1)
+        wallet_usdt1 = self.usdt.get_wallet(acc1)
+        
+        wallet_irt2 = self.irt.get_wallet(acc2)
+        wallet_usdt2 = self.usdt.get_wallet(acc2)
+        
+        wallet_irt3 = self.irt.get_wallet(acc3)
+        wallet_usdt3 = self.usdt.get_wallet(acc3)
+
+        wallet_irt1.airdrop(3)
+        wallet_usdt2.airdrop(2000)
+
+        lock_key0 = uuid4()
+
+        lock_key1 = uuid4()
+        lock_key2 = uuid4()
+
+        with WalletPipeline() as pipeline:
+            pipeline.new_lock(lock_key0, wallet_usdt2, amount=1800, reason=WalletPipeline.TRADE)
+
+        with WalletPipeline() as pipeline:
+            pipeline.new_lock(lock_key1, wallet_irt1, amount=2, reason=WalletPipeline.TRADE)
+
+        with WalletPipeline() as pipeline:
+            pipeline.new_lock(lock_key2, wallet_irt1, amount=1, reason=WalletPipeline.TRADE)
+
+        with WalletPipeline() as pipeline:
+            pipeline.release_lock(lock_key1)
+            pipeline.release_lock(lock_key0, 1200)
+            trade_key = uuid4()
+            pipeline.new_trx(wallet_usdt2, wallet_usdt1, 1200, Trx.TRADE, trade_key)
+            pipeline.new_trx(wallet_irt1, wallet_irt2, 2, Trx.TRADE, trade_key)
+            pipeline.new_trx(wallet_usdt1, wallet_usdt3, Decimal('2.4'), Trx.COMMISSION, trade_key)
+            pipeline.new_trx(wallet_irt2, wallet_irt3, 0, Trx.COMMISSION, trade_key)
+
+            pipeline.release_lock(lock_key2)
+            pipeline.release_lock(lock_key0, 600)
+            trade_key = uuid4()
+            pipeline.new_trx(wallet_usdt2, wallet_usdt1, 600, Trx.TRADE, trade_key)
+            pipeline.new_trx(wallet_irt1, wallet_irt2, 37130400, Trx.TRADE, trade_key)
+            pipeline.new_trx(wallet_usdt1, wallet_usdt3, Decimal('1.2'), Trx.COMMISSION, trade_key)
+            pipeline.new_trx(wallet_irt2, wallet_irt3, 0, Trx.COMMISSION, trade_key)
+
