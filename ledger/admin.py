3a55e42051b2593a9d50d63f4a38f9a40903cabc
Reza <reza.abbasi.sut@gmail.com>
2024-07-17 12:25:49 +0330

    fix admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -590,7 +590,7 @@ class TransferAdmin(SimpleHistoryAdmin, AdvancedAdmin):
     readonly_fields = (
         'deposit_address', 'network', 'wallet', 'created', 'accepted_datetime', 'finished_datetime', 'get_risks',
         'out_address', 'memo', 'amount', 'irt_value', 'usdt_value', 'deposit', 'group_id', 'login_activity',
-        'address_book', 'accepted_by'
+        'address_book', 'accepted_by', 'block_number', 'last_block_number'
     )
     exclude = ('risks',)
 
