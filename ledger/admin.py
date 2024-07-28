281ddeff7a41a1901165405367081ac0f3b05f9e
Sajjad-Khoshdooni <sijalkhf@gmail.com>
2024-07-28 14:34:04 +0330

    fix

--- ledger/admin.py
+++ ledger/admin.py
@@ -844,14 +844,6 @@ class AddressKeyAdmin(admin.ModelAdmin):
     readonly_fields = ('address', 'account', 'memo')
     search_fields = ('address', 'public_address', 'account__user__phone', 'memo')
     list_filter = ('architecture', 'deleted', 'architecture')
-    actions = ('update_solana_trxs', )
-
-    @admin.action(description='Update Solana Trxs', permissions=['view'])
-    def update_solana_trxs(self, request, queryset):
-        from ledger.requester.address_requester import AddressRequester
-        requester = AddressRequester()
-        for q in queryset.filter(architecture='SOL'):
-            requester.refresh_solana_transactions(address=q.address, architecture=q.architecture)
 
 
 @admin.register(models.AssetSpreadCategory)
