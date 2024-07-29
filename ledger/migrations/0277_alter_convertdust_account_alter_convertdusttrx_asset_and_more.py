1902b3c8fe8426aa98fe46cdf566d6a355a71bc1
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-07-29 14:09:18 +0330

    fix: 🐛 migrations

--- ledger/migrations/0277_alter_convertdust_account_alter_convertdusttrx_asset_and_more.py
+++ ledger/migrations/0277_alter_convertdust_account_alter_convertdusttrx_asset_and_more.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('ledger', '0276_convertdust_convertdusttrx_and_more'),
+        ('ledger', '0277_alter_marginhistorymodel_amount'),
     ]
 
     operations = [
