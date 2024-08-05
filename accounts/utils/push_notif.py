c369f95eb9ca0069429315e8b70c3187f0af9a3c
Reza <reza.abbasi.sut@gmail.com>
2024-08-05 14:46:35 +0330

    enhance _get_access_token

--- accounts/utils/push_notif.py
+++ accounts/utils/push_notif.py
@@ -1,6 +1,7 @@
+import dataclasses
 import json
 import logging
-from typing import Tuple
+import time
 
 import requests
 from decouple import config
@@ -11,14 +12,35 @@ from accounts.models import User
 logger = logging.getLogger(__name__)
 
 
-def _get_access_token() -> Tuple[str, str]:
-    scopes = ['https://www.googleapis.com/auth/firebase.messaging']
-    firebase_dict = json.loads(config('FIREBASE_SECRET_JSON', ''))
+@dataclasses.dataclass
+class AccessToken:
+    time: float
+    project_id: str
+    token: str
 
-    credentials = ServiceAccountCredentials._from_parsed_json_keyfile(firebase_dict, scopes)
-    access_token_info = credentials.get_access_token()
 
-    return firebase_dict['project_id'], access_token_info.access_token
+_access_token = AccessToken(time=0, project_id="", token="")
+
+
+def _get_access_token() -> AccessToken:
+    global _access_token
+
+    now = time.time()
+
+    if now - _access_token.time >= 3600:
+        scopes = ['https://www.googleapis.com/auth/firebase.messaging']
+        firebase_dict = json.loads(config('FIREBASE_SECRET_JSON', ''))
+
+        credentials = ServiceAccountCredentials._from_parsed_json_keyfile(firebase_dict, scopes)
+        access_token_info = credentials.get_access_token()
+
+        _access_token = AccessToken(
+            time=now,
+            project_id=firebase_dict['project_id'],
+            token=access_token_info.access_token,
+        )
+
+    return _access_token
 
 
 def send_push_notif_to_user(user: User, title: str, body: str, image: str = None, link: str = None):
@@ -49,12 +71,12 @@ def send_push_notif(token: str, title: str, body: str, image: str = None, link: 
             }
         }
 
-    project_id, access_token = _get_access_token()
+    access_token = _get_access_token()
 
     resp = requests.post(
-        url=f'https://fcm.googleapis.com/v1/projects/{project_id}/messages:send',
+        url=f'https://fcm.googleapis.com/v1/projects/{access_token.project_id}/messages:send',
         headers={
-            'Authorization': 'Bearer ' + access_token,
+            'Authorization': 'Bearer ' + access_token.token,
             'Content-Type': 'application/json; UTF-8',
         },
         json={
