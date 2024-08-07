57e966546872cf85ddc09e2b8daf11dc523c0ecb
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-08-07 09:45:16 +0330

    fix: 🐛 limit otc notification

--- ledger/models/otc_trade.py
+++ ledger/models/otc_trade.py
@@ -118,12 +118,14 @@ class OTCTrade(models.Model):
         with WalletPipeline() as pipeline:
             for otc_trade in query_set:
                 pipeline.release_lock(otc_trade.group_id)
-                otc_trade.update(status=OTCTrade.EXPIRED)
+                otc_trade.status = OTCTrade.EXPIRED
+                otc_trade.save(update_fields=['status'])
                 symbol = otc_trade.otc_request.symbol.name
                 Notification.send(
                     recipient=otc_trade.otc_request.account.user,
                     title='سفارش قیمت ثابت منقضی شد',
-                    message=f'سفارش {symbol} شما منقضی شد'
+                    message=f'سفارش {symbol} شما منقضی شد',
+                    link="/trade/otc/history?tab=convert-history"
                 )
 
     @classmethod
@@ -276,7 +278,8 @@ class OTCTrade(models.Model):
             Notification.send(
                 recipient=self.otc_request.account.user,
                 title='سفارش قیمت ثابت شما انجام شد',
-                message=f'سفارش {symbol} شما انجام شد'
+                message=f'سفارش {symbol} شما انجام شد.',
+                link="/trade/otc/history?tab=convert-history"
             )
 
 
