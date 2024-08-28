eca64cd76a51be00dc34d8f6da9bd412c7f98e03
Reza <reza.abbasi.sut@gmail.com>
2024-08-28 18:38:28 +0330

    remove unused api

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -12,7 +12,6 @@ from .dashboard import dashboard
 from .change_password_view import ChangePasswordView
 from .quiz_passed_view import QuizPassedView
 from .email_verify_view import EmailOTPVerifyView, EmailVerifyView
-from .on_boarding_flow import OnBoardingFlowStatus
 from .change_phone import InitiateChangePhone, ChangePhoneView
 from .change_phone_before_verify import ChangePhoneBeforeVerifyView
 
