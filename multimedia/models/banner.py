75e579d3c3f7038a05a6a387b5b78d377969d9a9
Reza <reza.abbasi.sut@gmail.com>
2024-09-11 15:34:27 +0330

    fix minor

--- multimedia/models/banner.py
+++ multimedia/models/banner.py
@@ -7,7 +7,7 @@ class Banner(models.Model):
     ONLY_DESKTOP = 'only_desktop'
 
     title = models.CharField(max_length=64)
-    image = models.ImageField(storage=PublicMediaStorage())
+    image = models.ImageField(storage=PublicMediaStorage(), upload_to='banners/')
     link = models.CharField(max_length=256)
     app_link = models.CharField(max_length=256, blank=True)
     active = models.BooleanField(default=True)
