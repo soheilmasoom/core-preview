a0b534e4e3e83e7f3fa9b7e6de4395703b5d25db
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-09-11 17:11:25 +0330

    fix: 🐛 fcm push notif

--- ledger/views/asset_alert_view.py
+++ ledger/views/asset_alert_view.py
@@ -28,6 +28,15 @@ class AssetAlertCreateSerializer(serializers.ModelSerializer):
     def validate(self, data):
         user = self.context['request'].user
         asset = data['asset']
+        topic = f"price_alerts_{asset.symbol.lower()}"
+        manage_user_topic_subscription(user, topic, 'subscribe')
+        send_push_notif(
+            title="title",
+            body="message",
+            link=f'/price/btc',
+            topic=topic
+        )
+        logger.warning(f"retry-push {topic} -- user")
         if AssetAlert.objects.filter(user=user, asset=asset).exists():
             raise ValidationError({'asset': 'ارز دیجیتال انتخاب شده تحت‌نظر می‌باشد.'})
         if asset.is_cash():
