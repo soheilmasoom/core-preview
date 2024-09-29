863ba175f7dc18771a7e5b3bc74a4ee626ec40cf
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-09-29 16:57:12 +0330

    feat: 🎸 price change alert

--- ledger/views/asset_alert_rule_view.py
+++ ledger/views/asset_alert_rule_view.py
@@ -34,6 +34,7 @@ class AlertRuleUpdateSerializer(serializers.ModelSerializer):
 class AssetAlertRuleSerializer(serializers.ModelSerializer):
     description = serializers.CharField(required=False)
     remaining_alerts = serializers.SerializerMethodField()
+    base_asset = serializers.CharField(required=True)
 
     class Meta:
         model = AssetAlertRule
