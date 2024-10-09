3fc3204471b3db8d27f6553e1d676ace432dfe95
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-10-09 14:04:50 +0330

    feat: 🎸 internal withdraw

--- ledger/tasks/internal_transfer.py
+++ ledger/tasks/internal_transfer.py
@@ -5,20 +5,22 @@ from celery import shared_task
 from django.db import transaction
 from django.utils import timezone
 
-from ledger.models import InternalTransfer
+from ledger.models import InternalTransfer, ManualWithdraw
 from ledger.utils.fields import PROCESS, PENDING
 
 logger = logging.getLogger(__name__)
 
 @shared_task(queue='internal-transfer')
 def update_internal_transfers():
+    current_time = timezone.now()
+    freeze_time_threshold = current_time - timedelta(seconds=InternalTransfer.FREEZE_SECONDS)
+
     with transaction.atomic():
         pending_transfers = InternalTransfer.objects.select_for_update().filter(
             status=PENDING,
+            created__lte=freeze_time_threshold
         )
         for transfer in pending_transfers:
-            if transfer.in_freeze_time():
-                continue
             try:
                 transfer.accept()
                 transfer.alert_user()
