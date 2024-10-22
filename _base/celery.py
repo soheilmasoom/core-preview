5a3d81c1355d4dc5f84ab668c143e69a4921a05c
Afshin Hosseinalipour <webmaster.afshin@gmail.com>
2024-10-22 17:23:48 +0330

    fix: 🐛 celery code duplication

--- _base/celery.py
+++ _base/celery.py
@@ -324,14 +324,6 @@ app.conf.beat_schedule = {
             'expires': 3600
         },
     },
-    'check_conditional_price_alerts': {
-        'task': 'ledger.tasks.alert.check_conditional_price_alerts',
-        'schedule': crontab(minute=5),
-        'options': {
-            'queue': 'celery',
-            'expires': 3600
-        },
-    },
     'manage_user_topic_subscription': {
         'task': 'accounts.tasks.notification.manage_user_topic_subscription_task',
         'schedule': crontab(minute=5),
