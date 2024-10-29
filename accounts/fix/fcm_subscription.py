bea2294955d15dc40b48e408a7cba83f2b4294bb
Reza <reza.abbasi.sut@gmail.com>
2024-10-29 16:59:35 +0330

    fix script

--- accounts/fix/fcm_subscription.py
+++ accounts/fix/fcm_subscription.py
@@ -1,12 +1,8 @@
+from accounts.utils.fcm_topic import fcm_topic_manager
 from ledger.models import AssetAlert
 
 
 def subscribe_all():
     for asset_alert in AssetAlert.objects.order_by('id'):
-        user = asset_alert.user
-        topic = f"price_alerts_{asset_alert.asset.symbol.lower()}"
-        fcm_topic_subscription, created = FCMTopicSubscription.objects.get_or_create(
-            user=user,
-            topic=topic,
-            action='subscribe',
-        )
+        topic = AssetAlert.get_default_rule_push_topic(asset_alert.asset)
+        fcm_topic_manager.subscribe(topic, list(asset_alert.user.fcm_tokens.values_list('token', flat=True)))
