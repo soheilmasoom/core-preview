161e2fbb2c9baace586e85f65bb93a142c95ffdf
Reza <reza.abbasi.sut@gmail.com>
2024-11-07 17:37:14 +0330

    disable five_min alert due to bug

--- ledger/utils/auto_price_alert.py
+++ ledger/utils/auto_price_alert.py
@@ -263,7 +263,7 @@ def process_automated_price_alerts():
 
     # lowest has most priority
     altered_coins = {
-        **get_ratio_alerts(current_cycle_prices, current_cycle, symbol_to_asset, interval=AlertTrigger.FIVE_MIN),
+        # **get_ratio_alerts(current_cycle_prices, current_cycle, symbol_to_asset, interval=AlertTrigger.FIVE_MIN),
         **get_ratio_alerts(current_cycle_prices, current_cycle, symbol_to_asset, interval=AlertTrigger.ONE_HOUR),
         **get_ratio_alerts(current_cycle_prices, current_cycle, symbol_to_asset, interval=AlertTrigger.THREE_HOURS),
         **get_ratio_alerts(current_cycle_prices, current_cycle, symbol_to_asset, interval=AlertTrigger.SIX_HOURS),
