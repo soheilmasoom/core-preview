1774ed3bc4abe927d4b891574d1e9cd55a7d16ca
Reza <reza.abbasi.sut@gmail.com>
2024-11-14 14:21:47 +0330

    fix bug

--- accounts/utils/kaftar.py
+++ accounts/utils/kaftar.py
@@ -7,13 +7,11 @@ logger = logging.getLogger(__name__)
 
 
 def send_message(profile: str, text: str):
-    token = settings.KAFTAR_TOKEN
-
     if settings.DEBUG_OR_TESTING:
         print('send to %s' % profile)
         print(text)
 
-    if not profile or not token:
+    if not profile or not settings.KAFTAR_TOKEN:
         logger.info('No kaftar token set!')
         return
 
@@ -21,7 +19,7 @@ def send_message(profile: str, text: str):
         requests.post(
             f'{settings.KAFTAR_HOST_URL}/api/v1/messaging/send/',
             headers={
-                'Authorization': token
+                'Authorization': f'Token {settings.KAFTAR_TOKEN}'
             },
             data={
                 'profile': profile,
