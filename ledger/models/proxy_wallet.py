3f53823fa134112641c4e5a0a81816ce7f0176cf
Sajjad-Khoshdooni <sijalkhf@gmail.com>
2024-11-16 13:10:58 +0330

    refactor

--- ledger/models/proxy_wallet.py
+++ ledger/models/proxy_wallet.py
@@ -5,3 +5,7 @@ class ProxyWallet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     network = models.ForeignKey('ledger.Network', on_delete=models.PROTECT)
     address = models.CharField(max_length=256, unique=True)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.address},{self.network.symbol}'
