b49e7e6069cbc9f3a089dabbbc3f9b2f207fe128
Reza <reza.abbasi.sut@gmail.com>
2024-11-23 10:57:07 +0330

    fix bug

--- ledger/models/token_delist.py
+++ ledger/models/token_delist.py
@@ -57,7 +57,7 @@ class TokenDelist(models.Model):
         )
 
         coin = self.asset.symbol
-        jalali_date = str(gregorian_to_jalali_date(self.delist_at))
+        jalali_date = str(gregorian_to_jalali_date(self.delist_at.astimezone()))
 
         with transaction.atomic():
             for user in users:
