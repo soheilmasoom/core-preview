7c256cb3cba52bccb946af29887d33fcf6eb8e1e
Reza <reza.abbasi.sut@gmail.com>
2024-11-27 09:56:28 +0330

    disable jibit instant withdraw

--- financial/interface/jibit.py
+++ financial/interface/jibit.py
@@ -79,6 +79,8 @@ class JibitChannel(BaseChannel):
         )
 
     def get_instant_banks(self) -> list:
+        return []  # instant withdraw disabled
+
         swift_codes = self.collect_api('/v2/banks/status').data or []
 
         if not swift_codes:
