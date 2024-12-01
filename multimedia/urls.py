2a5d1fa90b089889f7d51ae1353a31e39805fa22
Reza <reza.abbasi.sut@gmail.com>
2024-12-01 15:58:41 +0330

    add caching to media apis

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -8,12 +8,12 @@ from multimedia.views.guide_view import GuideGroupView
 urlpatterns = [
     path('image/', ImageCreateView.as_view()),
     path('file/', FileCreateView.as_view()),
-    path('banners/', BannerListView.as_view()),
+    path('banners/', cache_page(300)(BannerListView.as_view())),
     path('faq/sections/', SectionsView.as_view()),
     path('faq/articles/<str:slug>/', ArticleView.as_view()),
     path('faq/articles/', ArticleSearchView.as_view()),
     path('faq/sections/<slug:slug>/recom/', PinnedArticlesView.as_view()),
-    path('faqs/<slug:slug>/', FAQByCategoryView.as_view()),
-    path('guides/<slug:slug>/', GuideGroupView.as_view()),
+    path('faqs/<slug:slug>/', cache_page(300)(FAQByCategoryView.as_view())),
+    path('guides/<slug:slug>/', cache_page(300)(GuideGroupView.as_view())),
     path('blog/posts/latest/', cache_page(3600)(LatestBlogPostsView.as_view())),
 ]
