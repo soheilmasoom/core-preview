6eb2df2ccd677050322a15d8f3c3f523914f2baf
Reza <reza.abbasi.sut@gmail.com>
2024-12-01 15:23:29 +0330

    fix migrations

--- financial/tasks/gateway.py
+++ financial/tasks/gateway.py
@@ -91,7 +91,6 @@ def check_withdraw_refunds():
                 if withdraw.status == DONE:
                     refund_done = withdraw.refund()
                     action_name = 'refunded'
-
                 else:
                     refund_done = withdraw.change_status(CANCELED)
                     action_name = 'canceled'
