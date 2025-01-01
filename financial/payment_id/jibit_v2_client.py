4b3cce09c6e904f3b96526479d0a2b74322ce982
Reza <reza.abbasi.sut@gmail.com>
2025-01-01 18:22:08 +0330

    add scope

--- financial/payment_id/jibit_v2_client.py
+++ financial/payment_id/jibit_v2_client.py
@@ -26,6 +26,7 @@ class JibitClientV2(JibitClient):
             json={
                 'apiKey': self.gateway.payment_id_api_key,
                 'secretKey': self.gateway.payment_id_secret,
+                'Scope': 'VARIZ_PID',
             },
             timeout=30,
         )
