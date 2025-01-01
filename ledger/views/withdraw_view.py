d8c990a069cee82aad134a2b5ab19ca476d4de5a
amirmkhsr <amirmkhsrv@gmail.com>
2025-01-01 17:25:26 +0330

    fix test fast buy

--- ledger/views/withdraw_view.py
+++ ledger/views/withdraw_view.py
@@ -74,15 +74,18 @@ class WithdrawSerializer(serializers.ModelSerializer):
 
         if attrs['address_book_id'] and from_panel:
             address_book = get_object_or_404(AddressBook, id=attrs['address_book_id'], account=account)
-            network = address_book.network
+            if address_book.type == AddressBook.INTERNAL:
+                receiver_phone = address_book.dest_user.phone
+            else:
+                network = address_book.network
 
-            if address_book.asset:
-                if asset != address_book.asset:
-                    raise ValidationError('دفترچه آدرس برای این برداشت معتبر نیست.')
+                if address_book.asset:
+                    if asset != address_book.asset:
+                        raise ValidationError('دفترچه آدرس برای این برداشت معتبر نیست.')
 
-            address = address_book.address
-            whitelist = address_book.whitelist
-            memo = address_book.memo
+                address = address_book.address
+                whitelist = address_book.whitelist
+                memo = address_book.memo
 
         else:
             if not asset:
