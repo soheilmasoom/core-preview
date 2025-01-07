feb660e7d93e1840fe8bc6882eee7e1fdc9419a5
amirmkhsr <amirmkhsrv@gmail.com>
2025-01-07 17:51:52 +0330

    fix minor

--- accounts/models/admin_tracker.py
+++ accounts/models/admin_tracker.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib import admin
+from accounts.admin_guard.html_tags import anchor_tag, admin_page_anchor
+from accounts.admin_guard.html_tags import url_to_edit_object
 from accounts.models import User
 
 
@@ -13,3 +15,18 @@ class AdminTracker(models.Model):
 
     def __str__(self):
         return f"{self.admin} {self.model_name} at {self.created}"
+
+    def admin_link(self, obj):
+        return admin_page_anchor(obj.admin, change=True) if obj.admin else "N/A"
+    admin_link.short_description = "Admin"
+
+    def user_link(self, obj):
+        return admin_page_anchor(obj.user, change=True) if obj.user else "N/A"
+    user_link.short_description = "User"
+
+    def object_link(self, obj):
+        if obj.object_id and obj.model_name:
+            dummy_object = type('DummyObject', (), {'_meta': type('Meta', (), {'app_label': 'app_name', 'model_name': obj.model_name.lower()}), 'id': obj.object_id})
+            return anchor_tag(obj.object_id, url_to_edit_object(dummy_object), target='_blank')
+        return "N/A"
+    object_link.short_description = "Object"
