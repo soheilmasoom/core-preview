c153f3ac3c31639bd96d16eb0db33a5a9306ff97
amirmkhsr <amirmkhsrv@gmail.com>
2025-01-12 17:04:15 +0330

    fix minor

--- ledger/utils/test.py
+++ ledger/utils/test.py
@@ -22,12 +22,10 @@ if settings.DEBUG_OR_TESTING:
     def get_rand_int():
         return random.randint(0, 100000000)
 
-
     def new_account() -> Account:
         name = 'test' + str(get_rand_int())
         u = User.objects.create(username=name, phone=name)
         return u.get_account()
-
 
     def set_price(asset: Asset, ask: float, bid: float = None):
         if not bid:
@@ -49,12 +47,10 @@ if settings.DEBUG_OR_TESTING:
 
         time.sleep(1)
 
-
     def set_up_user(self):
         phone = '09355913457'
         user = User.objects.create(username=phone, password='1', phone=phone)
         return user
-
 
     def generate_otp_code(user, scope) -> VerificationCode:
         otp_code = VerificationCode.objects.create(
@@ -64,7 +60,6 @@ if settings.DEBUG_OR_TESTING:
             user=user, )
         return otp_code.code
 
-
     def new_network(symbol: str = 'BSC') -> Network:
         symbol = symbol
         name = symbol
@@ -72,7 +67,6 @@ if settings.DEBUG_OR_TESTING:
         network = Network.objects.create(symbol=symbol, name=name, address_regex=address_regex)
 
         return network
-
 
     def new_network_asset(asset: Asset, network: Network):
 
@@ -96,9 +90,7 @@ if settings.DEBUG_OR_TESTING:
         )
         return network_asset
 
-
-    def new_address_book(account, network=None, asset=None, address='123', whitelist: bool = False,
-                         dest_user: User = None, address_type=AddressBook.TYPE_NETWORK) -> AddressBook:
+    def new_address_book(account, network = None, asset=None, address='123', whitelist: bool = False, dest_user: User=None, address_type=AddressBook.TYPE_NETWORK) -> AddressBook:
         name = 'test'
         address = address
         account = account
@@ -107,11 +99,9 @@ if settings.DEBUG_OR_TESTING:
         address_type = address_type
         if asset:
             asset = Asset.get(asset)
-        address_book = AddressBook.objects.create(name=name, address=address, account=account, network=network,
-                                                  asset=asset,
+        address_book = AddressBook.objects.create(name=name, address=address, account=account, network=network, asset=asset,
                                                   whitelist=whitelist, dest_user=dest_user, type=address_type)
         return address_book
-
 
     def new_deposit_address(account: Account, network: Network, address: str, memo: str = '') -> DepositAddress:
         address_key = AddressKey.objects.create(account=account, address=address, memo=memo)
@@ -121,16 +111,13 @@ if settings.DEBUG_OR_TESTING:
             address_key=address_key
         )
 
-
     def new_bankcard(user) -> BankCard:
-        bankcard = BankCard.objects.create(user=user, card_pan='1', verified=True, kyc=True, )
+        bankcard = BankCard.objects.create(user=user, card_pan='1', verified=True, kyc=True,)
         return bankcard
-
 
     def new_zibal_gateway() -> Gateway:
         gateway = Gateway.objects.create(name='test', type=Gateway.ZIBAL, merchant_id='zibal', active=True)
         return gateway
-
 
     def create_system_order_book(symbol: PairSymbol, side: str, data: list):
         with WalletPipeline() as pipeline:
@@ -143,7 +130,6 @@ if settings.DEBUG_OR_TESTING:
                     amount=d[1],
                     side=side
                 )
-
 
     def new_otc_request(account: Account = None):
         if not account:
@@ -162,7 +148,6 @@ if settings.DEBUG_OR_TESTING:
             market=Wallet.SPOT,
             order_type=OTCRequest.MARKET,
         )
-
 
     def new_trade_revenue(account: Account = None):
         return TradeRevenue.new(
