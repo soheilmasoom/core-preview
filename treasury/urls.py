8df8a507478682f98b33d672b31375f5aa5c5ae2
Saeed Masoumi <7masoumi@gmail.com>
2025-01-19 21:54:39 +0330

    Fix treasury endpoint

--- treasury/urls.py
+++ treasury/urls.py
@@ -4,7 +4,7 @@ from treasury.views import TreasuryListView, PhysicalWithdrawListView, PhysicalW
     PhysicalWithdrawInitView
 
 urlpatterns = [
-    path('treasury/', TreasuryListView.as_view(), name='treasury-list'),
+    path('', TreasuryListView.as_view(), name='treasury-list'),
     path('withdraw/', PhysicalWithdrawListView.as_view(), name='withdraw-list'),
     path('withdraw/<int:pk>/', PhysicalWithdrawDetailView.as_view(), name='withdraw-detail'),
     path('withdraw/init/', PhysicalWithdrawInitView.as_view(), name='withdraw-init'),
