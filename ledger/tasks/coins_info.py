8279331ab4821d988858f4255677237456be77e5
Saeed Masoumi <7masoumi@gmail.com>
2025-01-19 11:51:59 +0330

    populate_coins_info for precious metals

--- ledger/tasks/coins_info.py
+++ ledger/tasks/coins_info.py
@@ -1,10 +1,15 @@
 from datetime import timedelta
+from typing import List
 
 from celery import shared_task
 from django.core.cache import cache
 from django.utils import timezone
 
+from _base import settings
+from ledger.models import Asset
+from ledger.utils.external_price import fetch_external_price_by_symbol, SELL
 from ledger.utils.provider import get_provider_requester
+from ohlc.models import Ohlc1D, Ohlc1H
 
 
 @shared_task(queue='celery')
@@ -16,7 +21,10 @@ def populate_coins_info():
         return
 
     cache.set(fetch_key, 1, 60)
-    coins_info = get_provider_requester().get_coins_info()
+    if settings.EXCHANGE_TYPE.is_crypto:
+        coins_info = get_provider_requester().get_coins_info()
+    else:
+        coins_info = get_coins_info_from_ohlc()
 
     cache.set('coins_info', {
         'exp': (timezone.now() + timedelta(minutes=5)).timestamp(),
@@ -24,3 +32,35 @@ def populate_coins_info():
     }, timeout=72 * 3600)
 
     cache.delete(fetch_key)
+
+
+def get_coins_info_from_ohlc() -> List[dict]:
+    yesterday_23 = (timezone.now() - timedelta(days=1)).replace(hour=23, minute=0, second=0, microsecond=0)
+    assets = Asset.live_objects.exclude(symbol__in=["IRT", "USDT"])
+    data = []
+    for asset in assets:
+        pair = asset.symbol + 'IRT'
+        price = fetch_external_price_by_symbol(pair, SELL)
+        yesterday = Ohlc1H.objects.filter(symbol=pair, timestamp=yesterday_23).first()
+        change_24h = 0
+        if yesterday and yesterday.close > 0:
+            change_24h = ((price - float(yesterday.close)) / float(yesterday.close)) * 100
+
+        coin_data = {
+            'coin': asset.symbol,
+            'change_24h': change_24h,
+            'price': price,
+            'volume_24h': 0,
+            'change_7d': 0,
+            'high_24h': 0,
+            'low_24h': 0,
+            'change_1h': 0,
+            'cmc_rank': 0,
+            'market_cap': 0,
+            'circulating_supply': 0,
+            'weekly_trend_url': ''
+        }
+
+        data.append(coin_data)
+
+    return data
