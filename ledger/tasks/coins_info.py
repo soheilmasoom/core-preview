0ca5836f90c78b97cb341f50448c765bd08f82e4
Saeed Masoumi <7masoumi@gmail.com>
2025-01-19 13:52:43 +0330

    Use iran time

--- ledger/tasks/coins_info.py
+++ ledger/tasks/coins_info.py
@@ -1,15 +1,18 @@
 from datetime import timedelta
+from decimal import Decimal, ROUND_DOWN
 from typing import List
 
+import pytz
 from celery import shared_task
 from django.core.cache import cache
 from django.utils import timezone
+from django.utils.timezone import now
 
 from _base import settings
 from ledger.models import Asset
 from ledger.utils.external_price import fetch_external_price_by_symbol, SELL
 from ledger.utils.provider import get_provider_requester
-from ohlc.models import Ohlc1D, Ohlc1H
+from ohlc.models import Ohlc1H
 
 
 @shared_task(queue='celery')
@@ -35,7 +38,8 @@ def populate_coins_info():
 
 
 def get_coins_info_from_ohlc() -> List[dict]:
-    yesterday_23 = (timezone.now() - timedelta(days=1)).replace(hour=23, minute=0, second=0, microsecond=0)
+    yesterday_23 = get_iran_yesterday_23()
+
     assets = Asset.live_objects.exclude(symbol__in=["IRT", "USDT"])
     data = []
     for asset in assets:
@@ -45,25 +49,25 @@ def get_coins_info_from_ohlc() -> List[dict]:
         if asset.symbol == 'XAUM':
             price *= 1000
         yesterday = Ohlc1H.objects.filter(symbol=pair, timestamp=yesterday_23).first()
-        change_24h = 0
+        change_24h = Decimal('0')
         if yesterday and yesterday.close > 0:
-            change_24h = ((float(price) - float(yesterday.close)) / float(yesterday.close)) * 100
+            change_24h = ((price - yesterday.close) / yesterday.close * Decimal('100')).quantize(Decimal('0.00000000'),
+                                                                                                 rounding=ROUND_DOWN)
 
         coin_data = {
             'coin': asset.symbol,
             'change_24h': change_24h,
-            'price': price,
-            'volume_24h': 0,
-            'change_7d': 0,
-            'high_24h': 0,
-            'low_24h': 0,
-            'change_1h': 0,
-            'cmc_rank': 0,
-            'market_cap': 0,
-            'circulating_supply': 0,
-            'weekly_trend_url': ''
         }
 
         data.append(coin_data)
 
     return data
+
+
+def get_iran_yesterday_23():
+    iran_tz = pytz.timezone('Asia/Tehran')
+    utc_now = now()
+    iran_now = utc_now.astimezone(iran_tz)
+    iran_yesterday_23 = (iran_now - timedelta(days=1)).replace(hour=23, minute=0, second=0, microsecond=0)
+    utc_yesterday_23 = iran_yesterday_23.astimezone(pytz.utc)
+    return utc_yesterday_23.replace(minute=0, second=0, microsecond=0)
