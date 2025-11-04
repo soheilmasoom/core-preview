d1be5cc47f486a23cd5ed6b4a8a649b8e97dbe56
Saeed Masoumi <7masoumi@gmail.com>
2025-11-04 12:03:54 +0330

    Fix

--- accounts/views/prize_view.py
+++ accounts/views/prize_view.py
@@ -14,11 +14,28 @@ class PrizeSerializer(serializers.ModelSerializer):
     voucher = serializers.SerializerMethodField()
 
     def update(self, prize: Prize, validated_data):
-        redeemed = validated_data['redeemed']
+        redeemed = validated_data.get('redeemed')
 
+        # Only trigger the wallet pipeline if redeemed is being set to True
+        # and the prize wasn't already redeemed
         if redeemed is True and not prize.redeemed:
             with WalletPipeline() as pipeline:
                 prize.build_trx(pipeline)
+            # build_trx already sets redeemed=True and saves it
+            # so we need to refresh the instance
+            prize.refresh_from_db()
+
+        # Update other fields if present (excluding 'redeemed' since build_trx handles it)
+        update_fields = []
+        for attr, value in validated_data.items():
+            if attr != 'redeemed' or redeemed is not True:
+                # Only set redeemed manually if it's being set to False
+                # or if we didn't call build_trx
+                setattr(prize, attr, value)
+                update_fields.append(attr)
+
+        if update_fields:
+            prize.save(update_fields=update_fields)
 
         return prize
 
